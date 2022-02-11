eb8b44b83923603d203196e3f1bbd1c697b22ef0
Edmond-J-A <edomondja@gmail.com>
2022-02-11 23:48:22 +0800

    finish CheckUserLogin
    Signed-off-by: Edmond-J-A <edomondja@gmail.com>

--- database/user.go
+++ database/user.go
@@ -1,45 +1,56 @@
 package database
 
-import "fmt"
+import (
+    "fmt"
+)
 
 type User struct {
-	ID       int64  `json:"id" xorm:"pk autoincr"`
-	Username string `json:"username" xorm:"varchar(50) notnull unique"`
-	Password string `json:"password" xorm:"varchar(50) notnull"`
-	Avatar   string `json:"avatar" xorm:"varchar(50) notnull"`
-	Exp      int64  `json:"exp" xorm:"bigint notnull"`
-	Coin     int64  `json:"coin" xorm:"bigint notnull"`
-	Level    int64  `json:"level" xorm:"bigint notnull"`
-	Banned   bool   `json:"banned" xorm:"bool notnull"`
+    ID       int64  `json:"id" xorm:"pk autoincr"`
+    Username string `json:"username" xorm:"varchar(50) notnull unique"`
+    Password string `json:"password" xorm:"varchar(50) notnull"`
+    Avatar   string `json:"avatar" xorm:"varchar(50) notnull"`
+    Exp      int64  `json:"exp" xorm:"bigint notnull"`
+    Coin     int64  `json:"coin" xorm:"bigint notnull"`
+    Level    int64  `json:"level" xorm:"bigint notnull"`
+    Banned   bool   `json:"banned" xorm:"bool notnull"`
+    Online   bool   `json:"online" xorm:"bool notnull"`
 }
 
 func (u *User) ToProtocolString() string {
-	ret := fmt.Sprintf("USER %s %s %d %d %d ", u.Username, u.Avatar, u.Level, u.Exp, u.Coin)
+    ret := fmt.Sprintf("USER %s %s %d %d %d ", u.Username, u.Avatar, u.Level, u.Exp, u.Coin)
 
-	items := u.GetItems()
+    items := u.GetItems()
 
-	var hasItem [ItemNumber]bool
-	for _, item := range items {
-		hasItem[item.Id] = true
-	}
+    var hasItem [ItemNumber]bool
+    for _, item := range items {
+        hasItem[item.Id] = true
+    }
 
-	for _, has := range hasItem {
-		if has {
-			ret += "1"
-		} else {
-			ret += "0"
-		}
-	}
+    for _, has := range hasItem {
+        if has {
+            ret += "1"
+        } else {
+            ret += "0"
+        }
+    }
 
-	return ret
+    return ret
 }
 
 func (u *User) GetItems() []Item {
-	// TODO: finish
-	return nil
+    // TODO: finish
+    return nil
 }
 
-func CheckUserLogin(uid int, password string) (*User, string) {
-	// TODO: finish CODE_ERROR & ACCOUNT_ERROR & OK
-	return nil, "ACCOUNT_ERROR"
+func CheckUserLogin(uid string, password string) (*User, string) {
+    user := &User{}
+    has, _ := engine.Where("ID = ?", uid).Get(user)
+
+    if has && user.Password == password {
+        return user, "USER"     //OK
+    } else if has && user.Password != password {
+        return nil, "CODE_ERROR"
+    } else {
+        return nil, "ACCOUNT_ERROR"
+    }
 }
