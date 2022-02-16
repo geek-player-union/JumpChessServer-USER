1d1124523fc47569614cf2fe51d599a39384a9cb
Kininaru <shiftregister233@outlook.com>
2022-02-16 15:04:59 +0800

    fix logic
    Signed-off-by: Kininaru <shiftregister233@outlook.com>

--- database/user.go
+++ database/user.go
@@ -1,93 +1,78 @@
 package database
 
 import (
-    "fmt"
+	"encoding/json"
 )
 
 type User struct {
-    ID       int64  `json:"id" xorm:"pk autoincr"`
-    Username string `json:"username" xorm:"varchar(50) notnull unique"`
-    Password string `json:"password" xorm:"varchar(50) notnull"`
-    Avatar   string `json:"avatar" xorm:"varchar(50) notnull"`
-    Exp      int64  `json:"exp" xorm:"bigint notnull"`
-    Coin     int64  `json:"coin" xorm:"bigint notnull"`
-    Level    int64  `json:"level" xorm:"bigint notnull"`
-    Banned   bool   `json:"banned" xorm:"bool notnull"`
-    Online   bool   `json:"online" xorm:"bool notnull"`
-    Rank     int64  `json:"rank" xorm:"bigint notnull"`
-    Assert   string `json:"assert" xorm:"varchar(50) notnull"`
+	ID       int64  `json:"id" xorm:"pk autoincr"`
+	Username string `json:"username" xorm:"varchar(50) notnull unique"`
+	Password string `json:"password" xorm:"varchar(50) notnull"`
+	Avatar   string `json:"avatar" xorm:"varchar(50) notnull"`
+	Exp      int64  `json:"exp" xorm:"bigint notnull"`
+	Coin     int64  `json:"coin" xorm:"bigint notnull"`
+	Level    int64  `json:"level" xorm:"bigint notnull"`
+	Banned   bool   `json:"banned" xorm:"bool notnull"`
+	Online   bool   `json:"online" xorm:"bool notnull"`
+	Rank     int64  `json:"rank" xorm:"bigint notnull"`
+	Assert   string `json:"assert" xorm:"varchar(50) notnull"`
 }
 
-func (u *User) ToProtocolString() string {
-    ret := fmt.Sprintf("USER %s %s %d %d %d ", u.Username, u.Avatar, u.Level, u.Exp, u.Coin)
-    items := u.GetItems()
+func (u *User) ToJsonString() string {
+	ret, err := json.Marshal(u)
+	if err != nil {
+		panic(err)
+	}
 
-    var hasItem [ItemNumber]bool
-    for _, item := range items {
-        hasItem[item.Id] = true
-    }
-
-    for _, has := range hasItem {
-        if has {
-            ret += "1"
-        } else {
-            ret += "0"
-        }
-    }
-
-    return ret
+	return string(ret)
 }
 
-func (u *User) GetItems() []Item {
-    // TODO: finish
-    return nil
-}
+func BuyItems(id int64, ItemId int) string {
+	user := &User{ID: id}
+	has, _ := engine.Get(user)
 
-func BetItems(uid string, password string, ItemId int) (bool, string) {
-    user := &User{}
-    has, _ := engine.Where("ID = ?", uid).Get(user)
+	if !has {
+		return "DATABASE_ERROR"
+	}
 
-    if has {
-        if user.Password == password && user.Assert[ItemId] == '0' && user.Coin >= Items[ItemId].Price {
-            AssertBytes := []byte(user.Assert)
-            AssertBytes[ItemId] = '1'
-            user.Assert =string(AssertBytes)
-            user.Coin-=Items[ItemId].Price
-            _, err := engine.Update(user)
-            if err != nil {
-                return false, "SERVER_ERROR"
-            }
-            return true, "BUY_DONE"
-        } else if user.Password != password {
-            return false, "CONFIRM_ERROR"
-        }else if user.Assert[ItemId] == '1' {
-            return false, "RECURRING_ERROR"
-        } else {
-            return false, "COIN_ERROR"
-        }
-    } else {
-        return false,"DATABASE_ERROR"
-    }
+	if user.Assert[ItemId] != '0' {
+		return "RECURRING_ERROR"
+	}
+
+	if user.Coin < Items[ItemId].Price {
+		return "COIN_NOT_ENOUGH"
+	}
+
+	assertBytes := []byte(user.Assert)
+	assertBytes[ItemId] = '1'
+	user.Assert = string(assertBytes)
+	user.Coin -= Items[ItemId].Price
+	_, err := engine.Update(user)
+	if err != nil {
+		return "DATABASE_ERROR"
+	}
+	
+	return "BUY_DONE"
 }
 
 func CheckUserLogin(uid string, password string) (*User, string) {
-    user := &User{}
-    has, _ := engine.Where("ID = ?", uid).Get(user)
+	user := &User{}
+	has, _ := engine.Where("ID = ?", uid).Get(user)
 
-    if has {
-        if user.Password == password && user.Online== false {
-            user.Online=true
-            _, err := engine.Update(user)
-            if err != nil {
-                return nil, "SERVER_ERROR"
-            }
-            return user, "USER"     //OK
-        } else if user.Password != password {
-            return nil, "CODE_ERROR"
-        } else {
-            return nil, "ONLINE_ERROR"
-        }
-    } else {
-        return nil, "ACCOUNT_ERROR"
-    }
+	if has {
+		if user.Password == password && user.Online == false {
+			user.Online = true
+			_, err := engine.Update(user)
+			if err != nil {
+				return nil, "SERVER_ERROR"
+			}
+			return user, "USER" //OK
+		} else if user.Password != password {
+			return nil, "CODE_ERROR"
+		} else {
+			return nil, "ONLINE_ERROR"
+		}
+	} else {
+		return nil, "ACCOUNT_ERROR"
+	}
 }
