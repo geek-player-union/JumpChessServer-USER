a659fb2f463ca88f2f4e889e70e840fa00a863c3
Kininaru <shiftregister233@outlook.com>
2022-02-16 19:35:03 +0800

    fix errors
    Signed-off-by: Kininaru <shiftregister233@outlook.com>

--- database/init.go
+++ database/init.go
@@ -3,16 +3,17 @@ package database
 import (
 	"fmt"
 	"github.com/geek-player-union/JumpChessServer-USER/config"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/go-xorm/xorm"
 )
 
 var engine *xorm.Engine
 
 func Init() {
-	dbConnectStr := fmt.Sprintf("%s:%s", config.MysqlUsername, config.MysqlPassword)
+	dbConnectStr := fmt.Sprintf("%s:%s@/%s?charset=utf8", config.MysqlUsername, config.MysqlPassword, config.MysqlDBName)
 
 	var err error
-	engine, err = xorm.NewEngine(config.MysqlDBName, dbConnectStr)
+	engine, err = xorm.NewEngine("mysql", dbConnectStr)
 	if err != nil {
 		panic(err)
 	}
