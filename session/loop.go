59f8f3b29118fd9f242b4f80d3abaee9c4e9adcb
Kininaru <shiftregister233@outlook.com>
2022-02-16 19:28:56 +0800

    simplified loop logic
    Signed-off-by: Kininaru <shiftregister233@outlook.com>

--- session/loop.go
+++ session/loop.go
@@ -8,32 +8,20 @@ import (
 func (s *session) loop() {
 	defer s.connection.Close()
 	buf := make([]byte, 512)
-	var err error
-	var n int
 	var message string
 	var command string
 	for !s.terminated {
-		n, err = s.connection.Read(buf)
-		if err != nil || n < 1 {
+		if n, err := s.connection.Read(buf); err != nil || n < 1 {
 			break
 		}
 		message += string(buf)
-
 		for strings.Index(message, "\n") >= 0 {
 			command = strings.Split(message, "\n")[0]
-			message = message[len(command):]
-			if message[0] == '\n' {
-				message = message[1:]
-			}
+			message = message[len(command)+1:]
 			retBytes, _ := json.Marshal(handle(command, s))
 			ret := string(retBytes) + "\n"
-			if len(ret) == 0 {
+			if _, err := s.connection.Write([]byte(ret)); err != nil {
 				s.terminated = true
-			} else {
-				_, err = s.connection.Write([]byte(ret))
-				if err != nil {
-					s.terminated = true
-				}
 			}
 		}
 	}
