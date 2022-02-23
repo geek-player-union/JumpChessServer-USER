2e11f13a4fd8fcb77ca936e83ad978ee28466f79
Edmond-J-A <edomondja@gmail.com>
2022-02-23 21:57:15 +0800

    type assertion change when login
    Signed-off-by: Edmond-J-A <edomondja@gmail.com>

--- session/login.go
+++ session/login.go
@@ -1,18 +1,16 @@
 package session
 
 import (
+	"fmt"
 	"github.com/geek-player-union/JumpChessServer-USER/database"
 )
 
 func handleLogin(d interface{}) (string, int64) {
-	data, ok := d.(struct {
-		Account string
-		Code    string
-	})
+	data, ok := d.(map[string]interface{})
 
 	if !ok {
 		return "SYNTAX_ERROR", -1
 	}
-
-	return database.CheckUserLogin(data.Account, data.Code)
+	fmt.Println(data)
+	return database.CheckUserLogin(data["account"].(string), data["code"].(string))
 }
